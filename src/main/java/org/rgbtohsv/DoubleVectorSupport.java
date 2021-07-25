4fedefd6b3f5d39d0811ea5b9e105e0c1d3bfed6
Clayton Walker <clayton.m.walker@gmail.com>
2021-07-25 16:37:47 -0600

    add double vector support

---
+++ src/main/java/org/rgbtohsv/DoubleVectorSupport.java
@@ -0,0 +1,22 @@
+package org.rgbtohsv;
+
+import jdk.incubator.vector.DoubleVector;
+import jdk.incubator.vector.VectorSpecies;
+
+import java.util.Arrays;
+
+public class DoubleVectorSupport {
+    public static DoubleVector fromSingle(VectorSpecies<Double> species, double single) {
+        double[] a = new double[species.length()];
+        Arrays.fill(a, single);
+        return DoubleVector.fromArray(species, a, 0);
+    }
+
+    public static DoubleVector indexVector(VectorSpecies<Double> species) {
+        double[] a = new double[species.length()];
+        for (int i = 0; i < a.length; i++) {
+            a[i] = i;
+        }
+        return DoubleVector.fromArray(species, a, 0);
+    }
+}
