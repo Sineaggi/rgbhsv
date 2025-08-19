7bd61ca5c9472314bda0ccc99f356b312d65e814
Clayton Walker <clayton.m.walker@gmail.com>
2025-08-19 01:06:23 -0600

    More 128-bit implementations

--- src/main/java/org/rgbhsv/Float128Transpose.java
+++ src/main/java/org/rgbhsv/Float128Transpose.java
@@ -4,6 +4,9 @@ import jdk.incubator.vector.FloatVector;
 import jdk.incubator.vector.VectorShuffle;
 import jdk.incubator.vector.VectorSpecies;
 
+/**
+ * Implementation based on <a href="https://fgiesen.wordpress.com/2013/07/09/simd-transposes-1/">SIMD transposes 1</a>
+ */
 public class Float128Transpose implements FloatTranspose {
     private static final VectorSpecies<Float> SPECIES_128 = FloatVector.SPECIES_128; // A species for 4 floats
 
@@ -13,13 +16,30 @@ public class Float128Transpose implements FloatTranspose {
     public static final VectorShuffle<Float> interleave64_lft = VectorShuffle.fromValues(SPECIES_128, 0, 1, 8, 9);
     public static final VectorShuffle<Float> interleave64_rgt = VectorShuffle.fromValues(SPECIES_128, 2, 3, 10, 11);
 
-    // todo: write transpose function
-    // on arm, we use 128 bit wide float vectors
-    // that's 4 floats. so 4x4 matrix
+    private Float128Transpose(int impl) {
+        this.impl = impl;
+    }
+
+    public static Float128Transpose create() {
+        return new Float128Transpose(0);
+    }
+
+    public static Float128Transpose create(int impl) {
+        return new Float128Transpose(impl);
+    }
+
+    private final int impl;
+
     @Override
-    public Result transpose(FloatVector row1, FloatVector row2, FloatVector row3, FloatVector row4) {
-        // Step 1: Interleave elements of row1 and row2
-        ////FloatVector col4 = FloatVector.recombine(SPECIES_128, temp2, temp4, 1, 1);
+    public FloatTranspose.Result transpose(FloatVector row1, FloatVector row2, FloatVector row3, FloatVector row4) {
+        if (impl == 0) {
+            return interleave1(row1, row2, row3, row4);
+        } else {
+            return interleave2(row1, row2, row3, row4);
+        }
+    }
+
+    private FloatTranspose.Result interleave1(FloatVector row1, FloatVector row2, FloatVector row3, FloatVector row4) {
         var a0 = row1.rearrange(interleave32_lft, row2);
         var a1 = row1.rearrange(interleave32_rgt, row2);
         var a2 = row3.rearrange(interleave32_lft, row4);
@@ -30,9 +50,20 @@ public class Float128Transpose implements FloatTranspose {
         var Z = a1.rearrange(interleave64_lft, a3);
         var W = a1.rearrange(interleave64_rgt, a3);
 
-        // Now col1, col2, col
-        return new Result(
-                X, Y, Z, W
-        );
+        return new FloatTranspose.Result(X, Y, Z, W);
+    }
+
+    private FloatTranspose.Result interleave2(FloatVector p0, FloatVector p1, FloatVector p2, FloatVector p3) {
+        var b0 = p0.rearrange(interleave32_lft, p2);
+        var b1 = p1.rearrange(interleave32_lft, p3);
+        var b2 = p0.rearrange(interleave32_rgt, p2);
+        var b3 = p1.rearrange(interleave32_rgt, p3);
+
+        var X = b0.rearrange(interleave32_lft, b1);
+        var Y = b0.rearrange(interleave32_rgt, b1);
+        var Z = b2.rearrange(interleave32_lft, b3);
+        var W = b2.rearrange(interleave32_rgt, b3);
+
+        return new FloatTranspose.Result(X, Y, Z, W);
     }
 }
--- src/test/java/org/rgbhsv/Float128TransposeTests.java
+++ src/test/java/org/rgbhsv/Float128TransposeTests.java
@@ -13,7 +13,22 @@ public class Float128TransposeTests {
         var p2 = FloatVector.fromArray(FloatVector.SPECIES_128, new float[]{8, 9, 10, 11}, 0);
         var p3 = FloatVector.fromArray(FloatVector.SPECIES_128, new float[]{12, 13, 14, 15}, 0);
 
-        var out = new Float128Transpose().transpose(p0, p1, p2, p3);
+        var out = Float128Transpose.create().transpose(p0, p1, p2, p3);
+
+        assertEquals(FloatVector.fromArray(FloatVector.SPECIES_128, new float[]{0, 4, 8, 12}, 0), out.col1());
+        assertEquals(FloatVector.fromArray(FloatVector.SPECIES_128, new float[]{1, 5, 9, 13}, 0), out.col2());
+        assertEquals(FloatVector.fromArray(FloatVector.SPECIES_128, new float[]{2, 6, 10, 14}, 0), out.col3());
+        assertEquals(FloatVector.fromArray(FloatVector.SPECIES_128, new float[]{3, 7, 11, 15}, 0), out.col4());
+    }
+
+    @Test
+    public void test2() {
+        var p0 = FloatVector.fromArray(FloatVector.SPECIES_128, new float[]{0, 1, 2, 3}, 0);
+        var p1 = FloatVector.fromArray(FloatVector.SPECIES_128, new float[]{4, 5, 6, 7}, 0);
+        var p2 = FloatVector.fromArray(FloatVector.SPECIES_128, new float[]{8, 9, 10, 11}, 0);
+        var p3 = FloatVector.fromArray(FloatVector.SPECIES_128, new float[]{12, 13, 14, 15}, 0);
+
+        var out = Float128Transpose.create(-1).transpose(p0, p1, p2, p3);
 
         assertEquals(FloatVector.fromArray(FloatVector.SPECIES_128, new float[]{0, 4, 8, 12}, 0), out.col1());
         assertEquals(FloatVector.fromArray(FloatVector.SPECIES_128, new float[]{1, 5, 9, 13}, 0), out.col2());
